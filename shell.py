41ed8367258bb340eb598535a80059dda8a964c8
seanthony <sean@basecampcodingacademy.org>
2017-12-26 13:32:41 -0600

    created filename entry for shell. will implement a try-except for incorrect filenames next.

--- shell.py
+++ shell.py
@@ -1,5 +1,30 @@
+from core import FilterPhoto
+import os
+
+
+def clear_screen():
+    os.system('cls')  # windows
+    os.system('clear')  # linux / mac
+
+
+def sys_exit():
+    print('thank you for using the software. have a nice day.')
+    exit()
+
+
 def main():
-    return None
+    clear_screen()
+    print('hello world.\nwelcome to sean\'s photo editor.\nupload a picture of your choice and apply the filter of your choice.\nenter "q" to quit.')
+    while True:
+        filename = input(
+            'please enter the filename with path of the picture you are filtering: ').strip()
+        if filename.lower() == 'q':
+            sys_exit()
+        elif filename == '':
+            image = FilterPhoto('moto.jpg')
+            break
+        else:
+            image = FilterPhoto(filename)
 
 
 if __name__ == '__main__':
